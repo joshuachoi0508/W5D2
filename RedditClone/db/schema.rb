b9787ae3dc32d1174041a6e4862f3d1d766ea72f
App Academy Student <appacademy@Mac-mini.local>
2018-11-20 11:55:46 -0800

    Migrate Post table

--- RedditClone/db/schema.rb
+++ RedditClone/db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_20_191819) do
+ActiveRecord::Schema.define(version: 2018_11_20_195051) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "url"
+    t.text "content"
+    t.integer "sub_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["sub_id"], name: "index_posts_on_sub_id"
+    t.index ["title"], name: "index_posts_on_title"
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "subs", force: :cascade do |t|
     t.string "title", null: false
